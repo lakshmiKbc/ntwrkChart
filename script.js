b108889ab2e3ed3177939e26490e887952699b8e
Lakshmi S <lakshmi.s@gramener.org>
2020-12-04 06:07:19 +0530

    filters added

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 var app = new Vue({
-    el: '#chart'
-    // data: {
-    //   message: 'Hello Vue!'
-    // }
-  })
+  el: '#chart'
+  // data: {
+  //   message: 'Hello Vue!'
+  // }
+})
+
+new Vue({
+  el: '#song_select',
+  data: {
+    selected: ''
+  }
+})
